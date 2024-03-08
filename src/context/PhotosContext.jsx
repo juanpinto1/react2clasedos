c37891a4d64bdc871f08033e41e45b14e1e4b4c8
juanpinto1 <juan.pinto@cajalosandes.cl>
2024-03-07 22:18:15 -0300

    hola4

--- src/context/PhotosContext.jsx
+++ src/context/PhotosContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 export const PhotosContext = createContext();
 
-const PHOTO_URL = '/photos.json';
+const PHOTO_URL = './photos.json';
 
 const PhotosProvider = ({ children }) => {
 
